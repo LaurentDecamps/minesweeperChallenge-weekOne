0c04022d1d4e317982a9eb20c231657b10eb199d
Laurent Décamps <decamps.laurent@gmail.com>
2025-04-05 21:08:20 +0200

    test: make test6 pass

--- MineSweeperTest/MineSweeperTest.cs
+++ MineSweeperTest/MineSweeperTest.cs
@@ -61,7 +61,7 @@ public class MineSweeper
 
         var solution = field.Replace('.', '0');
 
-        if (field is ".*" or "*.")
+        if (field is ".*" or "*." or ".*.")
         {
             solution = solution.Replace('0', '1');
         }
